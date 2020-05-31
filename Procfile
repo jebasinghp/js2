04f01bc4c303601bec12bd3451f85dc755bd7e8f
Jeba Singh P <jebasinghp.2016.cse@ritchennai.edu.in>
2020-05-31 13:31:52 +0530

    w

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bokeh serve --port=$PORT --address=0.0.0.0 --allow-websocket-origin=tobacco-tax-viz.herokuapp.com --use-xheaders bokah.py
+web: bokeh serve --port=$PORT --allow-websocket-origin=linemain.herokuapp.com --address=0.0.0.0 --use-xheaders main.py
---
+++ main.py
@@ -0,0 +1,21 @@
+from bokeh.io import curdoc
+import pandas as pd
+import numpy as np 
+from bokeh.layouts import widgetbox, column,row
+
+from bokeh.plotting import figure, show
+
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+# output to static HTML file
+#output_file("lines.html")
+
+# create a new plot with a title and axis labels
+p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+# add a line renderer with legend and line thickness
+p.line(x, y, legend_label="Temp.", line_width=2)
+
+curdoc().add_root(column(p))
